6cd1d6a990ff551468d8c46393270900a2043df9
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-16 22:22:12 +0900

    feat: 총 벌어들인 금액을 계산하는 메서드 추가 및 테스트 작성

--- src/main/java/lotto/LottoHierarchy.java
+++ src/main/java/lotto/LottoHierarchy.java
@@ -14,6 +14,25 @@ public class LottoHierarchy {
     public LottoHierarchy() {
     }
 
+    public LottoHierarchy(boolean forTest) {
+        Lotto lotto1 = new Lotto(List.of(8, 21, 23, 41, 42, 43));
+        Lotto lotto2 = new Lotto(List.of(3, 5, 11, 16, 32, 38));
+        Lotto lotto3 = new Lotto(List.of(7, 11, 16, 35, 36, 44));
+        Lotto lotto4 = new Lotto(List.of(1, 8, 11, 31, 41, 42));
+        Lotto lotto5 = new Lotto(List.of(13, 14, 16, 38, 42, 45));
+        Lotto lotto6 = new Lotto(List.of(7, 11, 30, 40, 42, 43));
+        Lotto lotto7 = new Lotto(List.of(2, 13, 22, 32, 38, 45));
+        Lotto lotto8 = new Lotto(List.of(1, 3, 5, 14, 22, 45));
+        printedLotto.add(lotto1);
+        printedLotto.add(lotto2);
+        printedLotto.add(lotto3);
+        printedLotto.add(lotto4);
+        printedLotto.add(lotto5);
+        printedLotto.add(lotto6);
+        printedLotto.add(lotto7);
+        printedLotto.add(lotto8);
+    }
+
     /* ui 에서 받을 때를 생각해보면, 입력된 숫자 6개를 리스트로 받을 것이다.
      * 이후 받은 리스트와 지출된 금액을 토대로 메서드를 호출할 것이다. */
     public LottoHierarchy(long price) {
@@ -22,32 +41,33 @@ public class LottoHierarchy {
     }
 
     public int howManyDuplicates(List<Integer> numbers, Lotto lotto) {
-        numbers.retainAll(lotto.getNumbers());
-        return numbers.size();
+        List<Integer> tmpNumbers = new ArrayList<>(numbers);
+        tmpNumbers.retainAll(lotto.getNumbers());
+        return tmpNumbers.size();
     }
 
     public Ranks mapDuplicatesToRanks(int duplicatesCount, boolean isBonus) {
-        if (duplicatesCount == 3) return Ranks.FIFTH_PLACE;
-        if (duplicatesCount == 4) return Ranks.FOURTH_PLACE;
-        if (duplicatesCount == 5) {
-            if (isBonus) return Ranks.SECOND_PLACE;
-            return Ranks.THIRD_PLACE;
-        }
-        if (duplicatesCount == 6) return Ranks.FIRST_PLACE;
-        return Ranks.NONE;
+        if (duplicatesCount < 3) return Ranks.values()[0];
+        if (duplicatesCount != 5) return Ranks.values()[duplicatesCount-2];
+        if (isBonus) return Ranks.values()[duplicatesCount];
+        return Ranks.values()[duplicatesCount-2];
     }
 
     /* 겹치는 것을 검사하는 메서드에 everyLottoGenerator 과 winningNumbers 를 태운다. */
-    public Ranks rankedWhereAbouts(List<Integer> winningNumbers, int bonus) {
+    public List<Ranks> rankedWhereAbouts(List<Integer> winningNumbers, int bonus) {
+        List<Ranks> statistics = new ArrayList<>();
         Ranks ranking = null;
         boolean isBonus = false;
         int duplicates = 0;
+
         for (Lotto lotto : printedLotto) {
             duplicates = howManyDuplicates(winningNumbers, lotto);
             isBonus = lotto.getNumbers().contains(bonus);
             ranking = mapDuplicatesToRanks(duplicates, isBonus);
+            statistics.add(ranking);
+
         }
-        return ranking;
+        return statistics;
     }
 
     public int generatedLottoNums(long price) {
@@ -77,5 +97,19 @@ public class LottoHierarchy {
         }
         printedLotto.add(lotto);
         return everyLottoGenerator(repetition);
+    }
+
+    public Long earnings(List<Ranks> rankings) {
+        Long profit = 0L;
+        for (Ranks ranking : rankings) {
+            profit += ranking.getValue();
+        }
+        return profit;
+    }
+
+    public void printLotto() {
+        for (Lotto lotto : printedLotto) {
+            System.out.println(lotto.toString());
+        }
     }
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,15 +1,32 @@
 package lotto;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+
+    LottoHierarchy lottoHierarchy;
+    List<Integer> winningNumbers;
+    @BeforeEach
+    void SetUp() {
+         lottoHierarchy = new LottoHierarchy();
+         winningNumbers= new ArrayList<>();
+         winningNumbers.add(1);
+         winningNumbers.add(2);
+         winningNumbers.add(3);
+         winningNumbers.add(4);
+         winningNumbers.add(5);
+         winningNumbers.add(6);
+    }
+
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -30,47 +47,30 @@ class LottoTest {
     @DisplayName("주어진 금액에 맞는 로또 개수를 찾는다.")
     @Test
     void howManyLotto() {
-        LottoHierarchy lottoHierarchy = new LottoHierarchy();
         Assertions.assertEquals(16, lottoHierarchy.generatedLottoNums(16000));
     }
 
-//    @DisplayName("무작위의 중복 없는 6개의 숫자를 포함하는 리스트를 생성한다.")
+//    @DisplayName("무작위의 중복 없는 6개의 숫자를 포함하는 리스트를 생성한다.") // 그런 기능 없음
 //    @Test
 //    void generateLotto() {
-//        List<Integer> sixNumbers = new Lotto(List.of(1, 2, 3, 4, 5, 6)).lottoGenerator();
-//        Assertions.assertEquals(sixNumbers.stream().distinct().count(), sixNumbers.size());
+//        LottoHierarchy lottoHierarchy = new LottoHierarchy();
+//        Lotto sixNumbers = lottoHierarchy.lottoGenerator();
+//        Assertions.assertEquals(sixNumbers.getNumbers().stream().distinct().count(), sixNumbers.getNumbers().size());
 //    }
 
     @DisplayName("주어진 개수만큼 로또를 생성한다.")
     @Test
     void generateANumberOfLotto() {
-        LottoHierarchy lottoHierarchy = new LottoHierarchy(8000);
-        List<Lotto> printedLotto = new ArrayList<>();
-        for (Lotto lotto : lottoHierarchy.everyLottoGenerator(8)) {
+        List<Lotto> severalLotto = lottoHierarchy.everyLottoGenerator(8);
+        for (Lotto lotto : severalLotto) {
             System.out.println(lotto.toString());
         }
+        Assertions.assertEquals(8, severalLotto.size());
     }
-//
-//    @DisplayName("등수를 정확히 판단한다.")
-//    @Test
-//    void findInWhichRanking() {
-//        LottoHierarchy lottoHierarchy = new LottoHierarchy(8000);
-//        List<Integer> winningNumbers = List.of(1,2,3,4,5,6);
-//        Assertions.assertEquals(1, lottoHierarchy.rankedWhereAbouts(winningNumbers));
-//    }
 
     @DisplayName("중복되는 원소의 개수를 정확히 판단한다.")
     @Test
     void findNumberOfDuplicates() {
-        LottoHierarchy lottoHierarchy = new LottoHierarchy();
-        List<Integer> toCompare = new ArrayList<>();
-        toCompare.add(1);
-        toCompare.add(2);
-        toCompare.add(3);
-        toCompare.add(4);
-        toCompare.add(5);
-        toCompare.add(6);
-
         List<Integer> toBeCompared = new ArrayList<>();
         toBeCompared.add(1);
         toBeCompared.add(3);
@@ -80,6 +80,25 @@ class LottoTest {
         toBeCompared.add(45);
         Lotto lotto = new Lotto(toBeCompared);
 
-        Assertions.assertEquals(3, lottoHierarchy.howManyDuplicates(toCompare, lotto));
+        Assertions.assertEquals(3, lottoHierarchy.howManyDuplicates(winningNumbers, lotto));
+    }
+
+    @DisplayName("당첨 등수의 목록을 정확히 반환한다.")
+    @Test
+    void getStatistics() {
+        LottoHierarchy lottoHierarchy = new LottoHierarchy(true);
+        List<Ranks> ranks = lottoHierarchy.rankedWhereAbouts(winningNumbers, 7);
+        Assertions.assertTrue(ranks.contains(Ranks.FIFTH_PLACE));
+    }
+
+    List<Ranks> getRankings() {
+        LottoHierarchy lottoHierarchy = new LottoHierarchy(true);
+        return lottoHierarchy.rankedWhereAbouts(winningNumbers, 7);
+    }
+
+    @DisplayName("얼마를 벌었는지 정확히 금액을 반환한다.")
+    @Test
+    void earnHowMuch() {
+        Assertions.assertEquals(5000L, lottoHierarchy.earnings(getRankings()));
     }
 }
