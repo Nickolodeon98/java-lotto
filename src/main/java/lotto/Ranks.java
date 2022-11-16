0c40f9df7b3d33752139b1283a808dc0de9d04ad
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-16 22:21:49 +0900

    feat: Ranks enum 에 각 등수별로 얼마를 벌어들일 수 있는지 명시

--- src/main/java/lotto/Ranks.java
+++ src/main/java/lotto/Ranks.java
@@ -2,7 +2,20 @@ package lotto;
 
 public enum Ranks {
     /* 변하지 않는 값을 Enum 을 적용해 표현할 수 있다. */
-    FIRST_PLACE, SECOND_PLACE, THIRD_PLACE, FOURTH_PLACE, FIFTH_PLACE, NONE
+    NONE(0L),
+    FIFTH_PLACE(5000L),
+    FOURTH_PLACE(50000L),
+    THIRD_PLACE(1500000L),
+    FIRST_PLACE(3000000L),
+    SECOND_PLACE(2000000000L);
 
+    private final Long value;
 
+    Ranks(Long value) {
+        this.value = value;
+    }
+
+    public Long getValue() {
+        return value;
+    }
 }
